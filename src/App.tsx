37bfb9420ce66bfa2cf3feec7383ee4e1951d4bc
Lucian Mocanu <lucian.mocanu@nexontis.com>
2020-03-21 16:14:22 +0100

    remove boilerplate and add UI Fabric components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Card } from '@uifabric/react-cards';
+import { Stack, Text } from 'office-ui-fabric-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack>
+      <Card>
+        <Card.Item>
+          <Text>Hello World!</Text>
+        </Card.Item>
+      </Card>
+    </Stack>
   );
 }
 
